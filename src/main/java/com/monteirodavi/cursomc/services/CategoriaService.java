af415cf78a70b80624c3c013a8a14b6eecc15c11
DMPMS <monteiro_davi@yahoo.com>
2020-06-17 14:53:13 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/monteirodavi/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.monteirodavi.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.monteirodavi.cursomc.domain.Categoria;
+import com.monteirodavi.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
