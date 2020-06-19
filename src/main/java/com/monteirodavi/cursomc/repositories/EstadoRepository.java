983656418de0a8c355926467f95269ca92b2c6b9
DMPMS <monteiro_davi@yahoo.com>
2020-06-19 11:04:05 -0300

    Estado e cidade

---
+++ src/main/java/com/monteirodavi/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.monteirodavi.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.monteirodavi.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
