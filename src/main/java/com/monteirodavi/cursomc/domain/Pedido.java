e3b7470f4e6614bd9ad7e9c78f5ed55b9724d5b2
DMPMS <monteiro_davi@yahoo.com>
2020-06-23 16:22:39 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/monteirodavi/cursomc/domain/Pedido.java
+++ src/main/java/com/monteirodavi/cursomc/domain/Pedido.java
@@ -15,116 +15,119 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Pedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Integer id;
-private Date instante;
+	private Integer id;
 
-@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
-private Pagamento pagamento;
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+	private Date instante;
 
-@ManyToOne
-@JoinColumn(name = "cliente_id")
-private Cliente cliente;
+	@JsonManagedReference
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
+	private Pagamento pagamento;
 
-@ManyToOne
-@JoinColumn(name = "endereco_de_entrega_id")
-private Endereco enderecoDeEntrega;
+	@JsonManagedReference
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
+	private Cliente cliente;
 
-@OneToMany(mappedBy = "id.pedido")
-private Set<ItemPedido> itens = new HashSet<>();
+	@ManyToOne
+	@JoinColumn(name = "endereco_de_entrega_id")
+	private Endereco enderecoDeEntrega;
 
-public Pedido() {
-	
-	
-}
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
 
-public Pedido(Integer id, Date instante, Cliente cliente, Endereco enderecoDeEntrega) {
-	super();
-	this.id = id;
-	this.instante = instante;
-	this.cliente = cliente;
-	this.enderecoDeEntrega = enderecoDeEntrega;
-}
+	public Pedido() {
 
-public Integer getId() {
-	return id;
-}
+	}
 
-public void setId(Integer id) {
-	this.id = id;
-}
+	public Pedido(Integer id, Date instante, Cliente cliente, Endereco enderecoDeEntrega) {
+		super();
+		this.id = id;
+		this.instante = instante;
+		this.cliente = cliente;
+		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
 
-public Date getInstante() {
-	return instante;
-}
+	public Integer getId() {
+		return id;
+	}
 
-public void setInstante(Date instante) {
-	this.instante = instante;
-}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-public Pagamento getPagamento() {
-	return pagamento;
-}
+	public Date getInstante() {
+		return instante;
+	}
 
-public void setPagamento(Pagamento pagamento) {
-	this.pagamento = pagamento;
-}
+	public void setInstante(Date instante) {
+		this.instante = instante;
+	}
 
-public Cliente getCliente() {
-	return cliente;
-}
+	public Pagamento getPagamento() {
+		return pagamento;
+	}
 
-public void setCliente(Cliente cliente) {
-	this.cliente = cliente;
-}
+	public void setPagamento(Pagamento pagamento) {
+		this.pagamento = pagamento;
+	}
 
-public Endereco getEnderecoDeEntrega() {
-	return enderecoDeEntrega;
-}
+	public Cliente getCliente() {
+		return cliente;
+	}
 
-public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
-	this.enderecoDeEntrega = enderecoDeEntrega;
-}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
 
-public Set<ItemPedido> getItens() {
-	return itens;
-}
+	public Endereco getEnderecoDeEntrega() {
+		return enderecoDeEntrega;
+	}
 
-public void setItens(Set<ItemPedido> itens) {
-	this.itens = itens;
-}
+	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
+		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
 
-@Override
-public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + ((id == null) ? 0 : id.hashCode());
-	return result;
-}
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
 
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	Pedido other = (Pedido) obj;
-	if (id == null) {
-		if (other.id != null)
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
 			return false;
-	} else if (!id.equals(other.id))
-		return false;
-	return true;
-}
-
-
-
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 }
